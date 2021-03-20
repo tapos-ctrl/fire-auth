4c05d64a594624422627eb5c1a048417f2d8fb8a
tapos-ctrl <taposchandro231@gmail.com>
2021-03-20 13:12:18 +0600

    Sign up from

--- src/App.js
+++ src/App.js
@@ -1,23 +1,188 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebes.confing';
+import { useState } from 'react';
+firebase.initializeApp(firebaseConfig)
 
 function App() {
+  const [newUser, setNewUser] = useState(false);
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    newUser: false,
+    name: '',
+    email: '',
+    passwor: '',
+    photoURL: '',
+  })
+
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  const fbProvider = new firebase.auth.FacebookAuthProvider();
+  const handleSignin = () => {
+    firebase.auth().signInWithPopup(googleProvider)
+      .then(res => {
+        console.log(res)
+        const { displayName, photoURL, email } = res.user;
+        const signInUser = {
+          isSignedIn: true,
+          name: displayName,
+          email: email,
+          phot: photoURL,
+        }
+        setUser(signInUser);
+        console.log(displayName, photoURL, email);
+      })
+      .catch(err => {
+        console.log(err);
+        console.log(err.message);
+      })
+  }
+
+  const handleFbSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(fbProvider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
+
+        // The signed-in user info.
+        var user = result.user;
+
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var accessToken = credential.accessToken;
+        console.log('fb user',user);
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+
+        // ...
+      });
+  }
+
+
+
+  const handleSignOut = () => {
+    firebase.auth().signOut()
+      .then(() => {
+        const signedOutUser = {
+          isSignedIn: false,
+          name: '',
+          photo: '',
+          email: '',
+          error: '',
+          success: false,
+        }
+        setUser(signedOutUser);
+      })
+      .catch((error) => {
+
+      });
+  }
+  const handleChange = (event) => {
+    console.log(event.target.name, event.target.value);
+    let isFieldValid = true;
+    if (event.target.name === 'email') {
+      isFieldValid = /\S+@\S+\.\S+/.test(event.target.value);
+
+    }
+    if (event.target.name === 'password') {
+      const isPasswordValid = event.target.value.length > 6;
+      const passwordNumber = /\d{1}/.test(event.target.value);
+
+      isFieldValid = (isPasswordValid && passwordNumber);
+    }
+    if (isFieldValid) {
+      const newUserInfo = { ...user };
+      newUserInfo[event.target.name] = event.target.value;
+      setUser(newUserInfo);
+    }
+  }
+  const handleSubmit = (e) => {
+    console.log(user.name, user.password)
+    if (newUser && user.email && user.password) {
+      firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+        .then(res => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = '';
+          newUserInfo.success = true;
+          setUser(newUserInfo);
+          updateUserName(user.name);
+          console.log('sign in user info', res.user);
+        })
+        .catch((error) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = error.message;
+          newUserInfo.success = false;
+          setUser(newUserInfo);
+
+        });
+    }
+    if (!newUser && user.email && user.password) {
+      firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(res => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = '';
+          newUserInfo.success = true;
+          setUser(newUserInfo);
+        })
+        .catch((error) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = error.message;
+          newUserInfo.success = false;
+          setUser(newUserInfo);
+        });
+    }
+    e.preventDefault();
+  }
+  const updateUserName = name => {
+    const user = firebase.auth().currentUser;
+
+    user.updateProfile({
+      displayName: name,
+    }).then(function () {
+      console.log('user name updated successfully')
+    }).catch(function (error) {
+      console.log(error);
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        user.isSignedIn ? <button onClick={handleSignOut}>Sign Out</button> :
+          <button onClick={handleSignin}>Sign in</button>
+      }
+      <br />
+      <button onClick={handleFbSignIn}>Log in using Facebook</button>
+      {
+        user.isSignedIn && <div>
+          <p>Welcome, {user.name}</p>
+          <p>Your email: {user.email}</p>
+          <img src={user.photo} alt=""></img>
+        </div>
+      }
+
+      <h1>Our Own Authentication</h1>
+      <input type="checkbox" onChange={() => setNewUser(!newUser)} name="newUser" id="" />
+      <label htmlFor="newUser">New User Sign up</label>
+      <form onSubmit={handleSubmit}>
+        {newUser && <input type="text" name="name" onBlur={handleChange} placeholder="Your Name" required />}
+        <br />
+        <input type="text" name="email" onBlur={handleChange} placeholder="Your Email address" required />
+        <br />
+        <input type="password" name="password" onBlur={handleChange} placeholder="Your Password" required />
+        <br />
+        <input type="submit" value={newUser ? 'Sign up' : 'Sign in'} />
+      </form>
+      <p style={{ color: 'red' }}>{user.error}</p>
+      {user.success && <p style={{ color: 'green' }}>User {newUser ? 'created' : 'Logged In'} successfully</p>}
     </div>
   );
 }
---
+++ src/firebes.confing.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDkC9P6Fs2Zqwo030UZd0tCbx6fjq0sDP8",
+    authDomain: "ema-john-simple-443d3.firebaseapp.com",
+    projectId: "ema-john-simple-443d3",
+    storageBucket: "ema-john-simple-443d3.appspot.com",
+    messagingSenderId: "936319788655",
+    appId: "1:936319788655:web:28e41b777857532fd3cf9a"
+  };
+  export default firebaseConfig;
